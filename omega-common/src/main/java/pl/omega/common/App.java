93fc42b12ae31c49a0f9a1c0eb4f78f39f50d50e
Marek Puchalski <marpuch@gmail.com>
2012-02-02 20:32:54 +0100

    Created projects using maven.

---
+++ omega-common/src/main/java/pl/omega/common/App.java
@@ -0,0 +1,13 @@
+package pl.omega.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
